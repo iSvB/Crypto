814f9d987b1535287c18dde26548e227fb86a896
isvb <i.burdukov@gmail.com>
2015-05-27 22:46:52 +0600

    Init

---
+++ Crypto/PrimeNumber.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using static System.Math;
+
+namespace Crypto
+{
+    /// <summary>
+    /// Простое число.
+    /// </summary>
+    public class PrimeNumber
+    {
+        #region Fields
+        
+        /// <summary>
+        /// Значение простого числа. 
+        /// </summary>
+        private BigInteger value;
+
+        #endregion
+        #region Constructors
+
+        /// <summary>
+        /// Создаёт случайное новое простое число принадлежащее указанному интервалу.
+        /// </summary>
+        /// <param name="minValue">Нижняя граница интервала (включённая).</param>
+        /// <param name="maxValue">Верхняя граница интервала (включённая).</param>
+        /// <param name="probability">Вероятность того, что число не простое.</param>
+        public PrimeNumber(BigInteger minValue, BigInteger maxValue, double probability)
+        {
+            // Количество требуемых раундов теста Миллера-Рабина.
+            int r = (int)Ceiling(-Log(probability, 4));
+        }
+
+        #endregion
+        #region Methods
+        #region Private
+
+        
+
+        #endregion
+        #region Public
+
+        public override string ToString()
+        {
+            return value.ToString();
+        }
+
+        #endregion
+        #endregion
+        #region Operators
+        #endregion
+    }
+}
---
+++ Crypto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Math;
+
+namespace Crypto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
